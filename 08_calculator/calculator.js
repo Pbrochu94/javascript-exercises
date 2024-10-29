d66ee7d3446b92c5f606840f861fd03b5e7e460b
Patrick Brochu <patrickbrochu70@gmail.com>
2024-10-29 14:01:18 -0400

    Done calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, current)=>total +current, 0) 
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, current)=>total * current, 1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let length = number;
+  if(number === 0)
+  {
+    return 1;
+  }
+  let arr= [];
+  for(let i = 0;i <length;i++)
+  {
+    arr[i]=number;
+    number--;
+  }
+  return arr.reduce((total, current)=> total*current)
 };
 
 // Do not edit below this line
@@ -31,3 +42,5 @@ module.exports = {
   power,
   factorial
 };
+
+
