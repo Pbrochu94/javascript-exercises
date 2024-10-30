3632a56f0595c3690e640d37e0d7f9087dc2e748
Patrick Brochu <patrickbrochu70@gmail.com>
2024-10-30 12:38:35 -0400

    Completed palindromes (besides last case)

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,10 +1,17 @@
 const palindromes = function (word) {
-    let arr = word.split("")
-    for (let i = 0; i < arr.length;i++)
+    let arr = word.toLowerCase().split("")
+    let onlyLetters = arr.filter((letter)=>(/[a-zA-Z]/).test(letter))
+    let reverseCounter = onlyLetters.length - 1
+    for (let i =0;i<onlyLetters.length-1;i++)
     {
-        
+        if(onlyLetters[i] !== onlyLetters[reverseCounter- i])
+        {
+            return false;
+        }
     }
+    return true;
 };
 
+console.log(palindromes('r3ace3car'))
 // Do not edit below this line
 module.exports = palindromes;
