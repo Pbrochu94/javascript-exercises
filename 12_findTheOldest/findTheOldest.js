94765ce0dfc9d9eee6d7148058b44cc132d6dd43
Patrick Brochu <patrickbrochu70@gmail.com>
2024-10-30 14:11:03 -0400

    Completed findTheOldest !

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,27 +1,10 @@
-const people = [
-    {
-      name: "Carly",
-      yearOfBirth: 1942,
-      yearOfDeath: 1970,
-    },
-    {
-      name: "Ray",
-      yearOfBirth: 1962,
-      yearOfDeath: 2011,
-    },
-    {
-      name: "Jane",
-      yearOfBirth: 1912,
-      yearOfDeath: 1941,
-    },
-  ]
 
 
 const findTheOldest = function(arrObject) {
+    arrObject.map((person)=>(person.yearOfDeath === undefined)?person["yearOfDeath"]= 2024:person.yearOfDeath)
     arrObject.sort((person1,person2)=>(person1.yearOfDeath-person1.yearOfBirth) > (person2.yearOfDeath-person2.yearOfBirth)?1:-1)
     return arrObject.pop()
 };
 
-console.log(findTheOldest(people))
 // Do not edit below this line
 module.exports = findTheOldest;
