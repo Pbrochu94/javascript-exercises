56bacabfc9a80c938723b333475c31f128a1a017
Patrick Brochu <patrickbrochu70@gmail.com>
2024-10-30 13:52:05 -0400

    completed testcase on findTheOldest exept the last
    Need to implement so people with no death date counts

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,27 @@
-const findTheOldest = function() {
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
 
+
+const findTheOldest = function(arrObject) {
+    arrObject.sort((person1,person2)=>(person1.yearOfDeath-person1.yearOfBirth) > (person2.yearOfDeath-person2.yearOfBirth)?1:-1)
+    return arrObject.pop()
 };
 
+console.log(findTheOldest(people))
 // Do not edit below this line
 module.exports = findTheOldest;
