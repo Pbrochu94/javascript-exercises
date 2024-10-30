90078a15a95945c26894d693934d61df5ac83e53
Patrick Brochu <patrickbrochu70@gmail.com>
2024-10-30 13:05:49 -0400

    First iteration of fibo with array.fill()

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let array = new Array(number)
+    array.fill(1,0,(number))
+    console.log(array)
+    for (let i = 0;i<array.length;i++)
+    {
+        if(i >= 2)
+        {
+            array[i] = array[i-1]+array[i-2];
+            console.log(array[i])
+        }
+    }
 };
 
+fibonacci(4)
 // Do not edit below this line
 module.exports = fibonacci;
