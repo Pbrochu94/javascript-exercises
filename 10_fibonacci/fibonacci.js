4574cb0bbb856c845d9273da7a66b710c46bbe25
Patrick Brochu <patrickbrochu70@gmail.com>
2024-10-30 13:27:02 -0400

    Done fibonacci with loops and push/pop

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,17 +1,24 @@
 const fibonacci = function(number) {
-    let array = new Array(number)
-    array.fill(1,0,(number))
-    console.log(array)
-    for (let i = 0;i<array.length;i++)
+    if(number == 0)
     {
+        return 0;
+    }
+    if(number < 0)
+    {
+        return "OOPS"
+    }
+    number = Number(number);
+    let array = [1];
+    for (let i = 1;i<number;i++)
+    {
+        array.push(1);
         if(i >= 2)
         {
             array[i] = array[i-1]+array[i-2];
-            console.log(array[i])
         }
     }
+    return array.pop()
 };
 
-fibonacci(4)
 // Do not edit below this line
 module.exports = fibonacci;
